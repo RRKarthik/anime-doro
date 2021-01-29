86f864c61a8304faddda87301197c7c43a95ff1e
RRKarthik <raviramkarthik7@gmail.com>
2021-01-29 12:35:26 +0530

    Total rewamp

--- app.js
+++ app.js
@@ -1,75 +1,76 @@
-let mainTime = 1;
-let breakTime = 2;
-let flag = 1;
-let myInt;
 const para = document.getElementById("timer");
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const reset = document.getElementById("reset");
 const audio = new Audio("sounds/Sparkle - Your Name.mp3");
-
-let mainCount = mainTime*60;
-let breakCount = breakTime*60; 
-function mainTimer()
+const work = document.getElementById("work");
+const rest = document.getElementById("break");
+let mFlag = 1;
+let bFlag = 1;
+let mainT;
+let breakT; 
+let flag = 1;
+let message;
+let counter = 1 * 60;
+function timer()
 {
-   
-   if(mainCount == "0"){
-    audio.play();
-    para.textContent = `00:00`;
-    if(alert("Time to watch anime!")){audio.pause();}
-    breakTimer();
-    mainCount = mainTime*60;
-    return;
-   }
-    let minutes = Math.floor(mainCount/60);
-    let seconds = mainCount % 60;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    para.textContent = `${minutes}:${seconds}`;
-    mainCount--; 
-
-}
-
-function breakTimer()
-{  
-   
-    if(breakCount == "0")
-   {
-       para.textContent = "00:00";
-       audio.play();
-       if(alert("Time to get back to work")){audio.stop();}
-       mainTimer();
-       breakCount = breakTime*60;
-       return;
-   }
-
-   let mins = Math.floor(breakCount/60);
-   let secs = breakCount%60;
-   mins = mins < 10 ? '0' + mins :mins; 
-   secs = secs < 10 ? '0' + secs : secs;
-   para.textContent = `${mins}:${secs}`;
-   breakCount--;
-
-}
-function starter()
-{
-    if(flag == 1 )
+    if(counter == "0")
     {
-         myInt = setInterval(mainTimer,1000);
+
+        para.textContent = "00:00";
+        if(flag == 1)
+        {
+            flag = 0;
+        audio.play();
+        audio.loop = false;
+        function after()
+        {
+            audio.pause();
+        }
+        if(confirm(message))
+        {
+            after();
+        }else{
+            after();
+        }
     }
-    flag = 0;
+       return; 
+    }
+   let minutes = Math.floor(counter/60);
+   let seconds = counter % 60;
+   minutes = minutes < 10 ? "0" + minutes : minutes;
+   seconds = seconds < 10 ? "0" + seconds : seconds;
+   para.textContent = `${minutes}:${seconds}`;
+   counter--;
+
 }
-function stopper()
+
+function mainSetter()
 {
-   clearInterval(myInt);
-   flag = 1;
+ if(mFlag == 1)
+ {
+     flag = 1;
+     message = "Go watch some anime";
+  clearInterval(breakT);
+  counter = 1 * 60;
+  mainT = setInterval(timer,1000);
+  mFlag = 0;
+  bFlag = 1;
+ }
 }
-function resetter()
-{
-    flag = 1;
-    mainCount = mainTime*60;
-    breakCount = breakTime*60; 
+
+function breakSetter()
+{  if(bFlag == 1)
+    {
+        flag = 1;
+        message = "Senpai wants you to get  back to work";
+    clearInterval(mainT);
+    counter = 2 * 60;
+    breakT = setInterval(timer,1000);
+    bFlag = 0;
+    mFlag = 1;
+    }
 }
-start.addEventListener("click",starter);
-stop.addEventListener("click",stopper);
-reset.addEventListener("click",resetter);
+
+work.addEventListener("click",mainSetter);
+rest.addEventListener("click",breakSetter);
